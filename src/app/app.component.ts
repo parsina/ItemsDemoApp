3b981e5a6bb71cf56b3889dc237d1b4f6daf934a
Javfar <javad.farzaneh.v@gmail.com>
2017-07-17 21:43:29 +1000

    Commit and Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Routes} from "@angular/router";
+import {ItemComponent} from "./components/item/item.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
 @Component({
   selector: 'app-root',
@@ -7,3 +11,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 }
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: ItemComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import {HttpModule} from "@angular/http";
 import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+
+import { AppComponent } from './app.component';
 import {ItemComponent} from "./components/item/item.component";
-import {ItemService} from "./components/item/item.service";
 import { HeaderComponent } from './components/header/header.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+
+import {ItemService} from "./components/item/item.service";
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: ItemComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Contact', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ItemService],
   bootstrap: [AppComponent]
